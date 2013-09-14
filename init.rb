17cec4cbd9c17a4ffd0402d2ca65ee08f2d2f935
ande3577 <ande3577@gmail.com>
2013-09-14 13:34:32 -0500

    Update author name

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_default_activities do
   settings :default => {'default_event_types' => "", :dummy_setting => ""}, :partial => 'settings/default_activities_settings'
   
   name 'Redmine Default Activities plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'David S Anderson'
+  description 'Allows selecting the default events to show in the activity view from the plugin settings'
   version '0.0.1'
   url 'https://github.com/ande3577/redmine_default_activities'
   author_url 'https://github.com/ande3577'
