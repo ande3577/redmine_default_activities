ea295ede170e85b3bf9e1e96c8ef2762d05793b6
ande3577 <ande3577@gmail.com>
2013-09-14 13:06:25 -0500

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_default_activities do
+  name 'Redmine Default Activities plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/ande3577/redmine_default_activities'
+  author_url 'https://github.com/ande3577'
+end
